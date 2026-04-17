a484f9a769a33f33c55c861032661985e601c067
Tharushi Kavindhya <tharushikavi29@gmail.com>
2026-04-17 07:26:57 +0530

    Add Initial frontend pages, components, and services structure

---
+++ frontend/src/pages/CreateTicket.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import API from "../api/axios";
+
+function CreateTicket() {
+  const [form, setForm] = useState({
+    title: "",
+    description: "",
+    category: "EQUIPMENT",
+    priority: "LOW",
+    createdBy: "user1",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await API.post("/tickets", form);
+      alert("Ticket Created");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Create Ticket</h2>
+
+      <input placeholder="Title"
+        onChange={(e) => setForm({ ...form, title: e.target.value })} />
+
+      <input placeholder="Description"
+        onChange={(e) => setForm({ ...form, description: e.target.value })} />
+
+      <button type="submit">Create</button>
+    </form>
+  );
+}
+
+export default CreateTicket;
---
+++ frontend/src/services/ticketService.js
@@ -0,0 +1,19 @@
+import API from "../api/axios";
+
+// Get tickets by technician
+export const getTicketsByTechnician = (name) =>
+  API.get(`/tickets/technician/${name}`);
+
+// Assign technician
+export const assignTechnician = (id, data) =>
+  API.put(`/tickets/${id}/assign`, null, { params: data });
+
+// Add comment
+export const addComment = (id, data) =>
+  API.post(`/tickets/${id}/comments`, data);
+
+// Upload images
+export const uploadImages = (id, formData) =>
+  API.post(`/tickets/${id}/upload`, formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
