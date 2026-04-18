4f2f88eb5c16fc622723e30059f7879baee43cf4
Sachin kalhara <kalharasachin25@gmail.com>
2026-04-18 17:35:08 +0530

    add user model

---
+++ backend/src/main/java/com/smartcampus/backend/model/User.java
@@ -0,0 +1,30 @@
+package com.smartcampus.backend.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Setter
+@Getter
+@Document("users")
+public class User {
+    @Id
+    private String id;
+    private String clerkUserId;
+    private String email;
+    private String name;
+    private List<String> roles;
+
+    public User() {}
+
+    public User(String clerkUserId, String email, String name) {
+        this.clerkUserId = clerkUserId;
+        this.email = email;
+        this.name = name;
+        this.roles = List.of("USER");
+    }
+
+}
