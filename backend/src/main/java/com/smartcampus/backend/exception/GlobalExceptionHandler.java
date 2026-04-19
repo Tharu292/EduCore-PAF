5553a5b5c4e3094937162b2e3098a3d19a47c7dc
sachinkalhara25 <sachinkalhara60@gmail.com>
2026-04-20 00:51:54 +0530

    update GlobalExceptionHandler

--- backend/src/main/java/com/smartcampus/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/smartcampus/backend/exception/GlobalExceptionHandler.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class  GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public Map<String, String> handle(Exception e) {
         return Map.of(
