79510dfbbdc315c224e2388d59ce9896bd0c9aa5
sachinkalhara25 <sachinkalhara60@gmail.com>
2026-04-20 00:48:58 +0530

    Add exception file-GlobalExceptionHandler

---
+++ backend/src/main/java/com/smartcampus/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.smartcampus.backend.exception;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public Map<String, String> handle(Exception e) {
+        return Map.of(
+                "error", e.getMessage()
+        );
+    }
+}
