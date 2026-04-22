95671ba6e84f3b417fa46fc9bb5e9a5c72978193
Nishu224 <nishadibatagalla08@gmail.com>
2026-04-22 12:51:45 +0530

    setup resource API integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import CreateTicket from "./pages/CreateTicket";
 import MyTickets from "./pages/MyTickets";
 import TechnicianTickets from "./pages/TechnicianTickets";
+import AdminDashboard from "./pages/AdminDashboard";
+import UserDashboard from "./pages/UserDashboard";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
+    <div className="mt-2">   {/* Optional padding under navbar */}
       <Routes>
         <Route path="/" element={<CreateTicket />} />
         <Route path="/my" element={<MyTickets />} />
         <Route path="/tech" element={<TechnicianTickets />} />
+
+        {/* 🔥 NEW DASHBOARDS */}
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/user" element={<UserDashboard />} />
+
+        <Route path="/resources" element={<AdminDashboard />} />
       </Routes>
+      </div>
     </BrowserRouter>
   );
 }
